a7db6950394b6f0ebf45c1ab188dee99f8538798
Michael Iglesias <mci12@my.fsu.edu>
2015-12-12 13:28:56 -0700

    added FIREBASE_URL constant.

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -5,6 +5,7 @@
   angular
     .module('eggnog')
     .constant('malarkey', malarkey)
-    .constant('moment', moment);
+    .constant('moment', moment)
+    .constant('FIREBASE_URL', 'https://eggnog.firebaseio.com/');
 
 })();
