8e323d4405622a83069250a9d208f8b3d51474ee
Nick Coury <nickcoury@gmail.com>
2015-12-12 16:39:18 -0700

    *Add note playing. *Style play screen.

--- src/app/mobile/play.controller.js
+++ src/app/mobile/play.controller.js
@@ -6,9 +6,24 @@
     .controller('PlayController', PlayController);
 
   /** @ngInject */
-  function PlayController(note, $state) {
+  function PlayController(note, $state, SoundsService) {
     var vm = this;
     vm.note = note;
+    var colors = [
+      'btn-darkred',
+      'btn-orange',
+      'btn-yellow',
+      'btn-success',
+      'btn-primary',
+      'btn-indigo',
+      'btn-violet',
+      'btn-lightred'
+    ];
+    vm.color = colors[note];
+
+    vm.playNote = function () {
+      SoundsService.playNote(vm.note);
+    };
 
     vm.back = function () {
       $state.go('mobile.select');
