67ccdada5cdb7bd924e8a29966210e7978b18065
Michael Iglesias <mci12@my.fsu.edu>
2015-12-12 13:09:14 -0700

    include angularfire.

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,18 @@
   'use strict';
 
   angular
-    .module('eggnog', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+    .module('eggnog', [
+      'ngAnimate',
+      'ngCookies',
+      'ngTouch',
+      'ngSanitize',
+      'ngMessages',
+      'ngAria',
+      'ngResource',
+      'ui.router',
+      'ui.bootstrap',
+      'toastr',
+      'firebase'
+    ]);
 
 })();
